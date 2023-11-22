118ff7b2d38d7ebd05b030fd301c7f8fe4e4293d
Dev Kunjadia <90876986+devk03@users.noreply.github.com>
2023-11-22 01:40:35 -0500

    initial auth is well on its way :)

--- server/main.py
+++ server/main.py
@@ -9,8 +9,7 @@ import os
 app = FastAPI()
 load_dotenv('../.env.local')
 client_id = os.getenv('SPOTIFY_CLIENT_ID')
-
-redirect_uri = 'http://localhost:8000/callback';
+client_secret = os.getenv('SPOTIFY_SECRET_KEY')
 
 
 @app.get("/")
@@ -24,14 +23,18 @@ def read_item(item_id: int, q: Union[str, None] = None):
 
 @app.get("/login")
 async def login():
+    '''Redirects to Spotify login page'''
+    redirect_uri = 'http://localhost:8000/login/callback'
+
+    # Generate a random string for the state parameter
     state = secrets.token_urlsafe(16)
-    scope = 'user-read-private user-read-email'
-    redirect_uri = 'http://localhost:3000/'
+
+    app_permissions = 'user-read-private user-read-email'
 
     query_params = {
         "response_type": "code",
         "client_id": client_id,
-        "scope": scope,
+        "scope": app_permissions,
         "redirect_uri": redirect_uri,
         "state": state
     }
@@ -40,6 +43,29 @@ async def login():
     auth_url = f'https://accounts.spotify.com/authorize?{query_string}'
 
     return RedirectResponse(url=auth_url)
+
+@app.get("/login/callback")
+async def callback(code: str = None, state: str = None):
+    if state is None:
+        raise HTTPException(status_code=400, detail="state_mismatch")
+
+    if code:
+        auth_header = base64.b64encode(f'{client_id}:{client_secret}'.encode()).decode()
+        token_url = 'https://accounts.spotify.com/api/token'
+        headers = {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Authorization': f'Basic {auth_header}'
+        }
+        payload = {
+            'code': code,
+            'redirect_uri': redirect_uri,
+            'grant_type': 'authorization_code'
+        }
+        async with httpx.AsyncClient() as client:
+            response = await client.post(token_url, headers=headers, data=payload)
+            return response.json()
+
+    raise HTTPException(status_code=400, detail="Invalid request")
 
 if __name__ == "__main__":
     import uvicorn
