6a715c278c4e2e37abfb196944aaa4032f899edc
Dev Kunjadia <90876986+devk03@users.noreply.github.com>
2023-12-29 00:29:20 -0500

    reading through coors documentation and adjusting

--- server/main.py
+++ server/main.py
@@ -230,6 +230,6 @@ if __name__ == "__main__":
     uvicorn.run(
         "main:app",
         host="0.0.0.0",
-        port=os.getenv("PORT", default=8000),
+        port=int(os.getenv("PORT", default=8000)),
         log_level="info",
     )
